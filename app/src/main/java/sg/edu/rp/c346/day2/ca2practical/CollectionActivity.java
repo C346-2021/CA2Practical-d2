d4072eee2f7424faf4812f5a6d9f907f9ba0cff6
jason-lim <jason_lim@rp.edu.sg>
2021-05-25 08:54:16 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/day2/ca2practical/CollectionActivity.java
@@ -0,0 +1,14 @@
+package sg.edu.rp.c346.day2.ca2practical;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CollectionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_collection);
+    }
+}
